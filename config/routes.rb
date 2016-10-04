7e86e2c5ab7e95387e4b99d93d4cdad79632a161
unknown <C14463202@BUS-3092-18.ict.ad.dit.ie>
2016-10-04 11:08:37 +0100

    Added Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+
+  get 'pages/home'
+
+  get 'pages/about'
+
   resources :movies
 
   resources :genres
@@ -7,7 +13,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
